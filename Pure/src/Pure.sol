95c4ba3a71bf84d8745dddd1b2c68fc0e5f5ebfb
Dominik Harz <dominik.harz@gmail.com>
2018-11-09 04:40:23 +0000

    Include equal and mul

--- Pure/src/Pure.sol
+++ Pure/src/Pure.sol
@@ -7,11 +7,31 @@ contract Pure {
 
     function add2(uint x, uint y) public pure returns (uint z) {
         z = x + y; 
-        require(z >= x);
+        require(z >= x, "overflow");
         return z;
     }
 
     function sub(uint256 a, uint256 b) public pure returns (uint256 z) {
         require((z = a - b) <= a, "underflow");
+    }
+
+    function mul(uint256 a, uint256 b) public pure returns (uint256 z) {
+        require((z = a * b) >= a, "overflow");
+    }
+
+    function smaller(uint256 a, uint256 b) public pure returns (bool) {
+        if (a < b) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function equal(uint256 a, uint256 b) public pure returns (bool) {
+        if (a == b) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
